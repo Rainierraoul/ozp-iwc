4a41a028fedeb26a8065a0ef4542bca02646e243
Mark Leone <mark.leone@axiosengineering.com>
2014-08-13 18:15:43 -0400

    initial checkinb of client api wrapper

--- app/js/client/client.js
+++ app/js/client/client.js
@@ -10,32 +10,32 @@ var ozpIwc=ozpIwc || {};
  * @param {boolean} [config.autoPeer=true] - Whether to automatically find and connect to a peer
  */
 ozpIwc.Client=function(config) {
-	config=config || {};
-	this.address="$nobody";
-	this.replyCallbacks={};
-	this.peerUrl=config.peerUrl;
-	var a=document.createElement("a");
-	a.href = this.peerUrl;
-	this.peerOrigin=a.protocol + "//" + a.hostname;
-	if(a.port)
-		this.peerOrigin+= ":" + a.port;
-	
-	
-	this.autoPeer=("autoPeer" in config) ? config.autoPeer : true;
-	this.msgIdSequence=0;
-	this.events=new ozpIwc.Event();
-	this.events.mixinOnOff(this);
-	this.receivedPackets=0;
-	this.receivedBytes=0;
-	this.sentPackets=0;
-	this.sentBytes=0;
-	this.startTime=ozpIwc.util.now();
-    this.window = window;
-	var self=this;
+    config=config || {};
+    this.address="$nobody";
+    this.replyCallbacks={};
+    this.peerUrl=config.peerUrl;
+    var a=document.createElement("a");
+    a.href = this.peerUrl;
+    this.peerOrigin=a.protocol + "//" + a.hostname;
+    if(a.port)
+        this.peerOrigin+= ":" + a.port;
 
-	if(this.autoPeer) {
-		this.findPeer();
-	}
+
+    this.autoPeer=("autoPeer" in config) ? config.autoPeer : true;
+    this.msgIdSequence=0;
+    this.events=new ozpIwc.Event();
+    this.events.mixinOnOff(this);
+    this.receivedPackets=0;
+    this.receivedBytes=0;
+    this.sentPackets=0;
+    this.sentBytes=0;
+    this.startTime=ozpIwc.util.now();
+    this.window = window;
+    var self=this;
+
+    if(this.autoPeer) {
+        this.findPeer();
+    }
 
     this.postMessageHandler = function(event) {
         if(event.origin !== self.peerOrigin){
@@ -53,8 +53,8 @@ ozpIwc.Client=function(config) {
             // ignore!
         }
     };
-	// receive postmessage events
-	this.messageEventListener=window.addEventListener("message", this.postMessageHandler, false);
+    // receive postmessage events
+    this.messageEventListener=window.addEventListener("message", this.postMessageHandler, false);
 };
 
 /**
@@ -83,31 +83,31 @@ ozpIwc.Client.prototype.receive=function(packet) {
  * false-like value.
  */
 ozpIwc.Client.prototype.send=function(fields,callback) {
-	var now=new Date().getTime();
-	var id="p:"+this.msgIdSequence++; // makes the code below read better
+    var now=new Date().getTime();
+    var id="p:"+this.msgIdSequence++; // makes the code below read better
 
-	var packet={
-		ver: 1,
-		src: this.address,
-		msgId: id,
-		time: now
-	};
+    var packet={
+        ver: 1,
+        src: this.address,
+        msgId: id,
+        time: now
+    };
 
-	for(var k in fields) {
-		packet[k]=fields[k];
-	}
+    for(var k in fields) {
+        packet[k]=fields[k];
+    }
 
-	if(callback) {
-		this.replyCallbacks[id]=callback;
-	}
-	var data=packet;
+    if(callback) {
+        this.replyCallbacks[id]=callback;
+    }
+    var data=packet;
     if (!ozpIwc.util.structuredCloneSupport()) {
         data=JSON.stringify(packet);
     }
-	this.peer.postMessage(data,'*');
-	this.sentBytes+=data.length;
-	this.sentPackets++;
-	return packet;
+    this.peer.postMessage(data,'*');
+    this.sentBytes+=data.length;
+    this.sentPackets++;
+    return packet;
 };
 
 /**
@@ -125,11 +125,11 @@ ozpIwc.Client.prototype.cancelCallback=function(msgId) {
 
 
 ozpIwc.Client.prototype.on=function(event,callback) {
-	if(event==="connected" && this.address !=="$nobody") {
-		callback(this);
-		return;
-	}
-	return this.events.on.apply(this.events,arguments);
+    if(event==="connected" && this.address !=="$nobody") {
+        callback(this);
+        return;
+    }
+    return this.events.on.apply(this.events,arguments);
 };
 
 ozpIwc.Client.prototype.off=function(event,callback) {
@@ -154,7 +154,6 @@ ozpIwc.Client.prototype.createIframePeer=function(peerUrl) {
         self.iframe.addEventListener("load",function() { self.requestAddress(); },false);
         document.body.appendChild(self.iframe);
         self.peer=self.iframe.contentWindow;
-
     };
     // need at least the body tag to be loaded, so wait until it's loaded
     if(document.readyState === 'complete' ) {
@@ -175,11 +174,62 @@ ozpIwc.Client.prototype.findPeer=function() {
 };
 
 ozpIwc.Client.prototype.requestAddress=function(){
-	// send connect to get our address
-	var self=this;
-	this.send({dst:"$transport"},function(message) {
-		self.address=message.dst;
-		self.events.trigger("connected",self);
+    // send connect to get our address
+    var self=this;
+    this.send({dst:"$transport"},function(message) {
+        self.address=message.dst;
+        self.events.trigger("connected",self);
         return null;//de-register callback
-	});
+    });
 };
+
+ozpIwc.Client.prototype.api=function(apiName) {
+    var wrapper=makeCommonWrapper();
+    switch(apiName) {
+        case 'names.api':
+            wrapper.api=ozpIwc.namesApi;
+        case 'system.api':
+            wrapper.api=ozpIwc.systemApi;
+            break;
+        case 'intents.api':
+            wrapper =augment(wrapper,['register','unregister','invoke','listen','broadcast'],invokeApi);
+            wrapper.api=ozpIwc.intentsApi;
+            break;
+        case 'data.api':
+            wrapper=augment(wrapper,['list','push','pop','unshift','shift'],invokeApi);
+            wrapper.api=ozpIwc.dataApi;
+            break;
+        default:
+            wrapper.error='Invalid API';
+    }
+    return wrapper;
+}
+
+var makeCommonWrapper=function() {
+    return augment({},['get','set','delete','watch','unwatch'],invokeApi)
+}
+
+var augment=function(obj,methods,callback) {
+    for (var m in methods) {
+        obj[m]=function() {
+            callback(this,m,arguments);
+        }
+    }
+}
+
+var invokeApi=function(self,action,resource,packet,callback) {
+    var resolveCB=function(){};
+    var rejectCB=function(){};
+    var p=new Promise(function(resolve,reject) {
+        resolveCB=resolve;
+        rejectCB=reject;
+    });
+    if (self.error) {
+        rejectCB(self.error);
+    } else {
+        var value=self.api.findOrMakeValue(packet);
+        var method="handle" + action.charAt(0).toUpperCase() + action.slice(1).toLowerCase();
+        self.api[method](...);
+    }
+    return p;
+}
